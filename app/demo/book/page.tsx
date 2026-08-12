f7de93c8aae63d68b0cbabd462eb3d40be51f026
Fredrik Nerlandsrem <fredrik@Fredriks-MacBook-Pro-2.local>
2026-08-12 23:35:05 +0200

    Switch demo booking to Cal.com

--- app/demo/book/page.tsx
+++ app/demo/book/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
-const FALLBACK_BOOKING_URL = "https://calendar.app.google/Wh2vYnLEm4KB3tHq5";
+const FALLBACK_BOOKING_URL = "https://cal.com/clovo/demo";
 
 export default function DemoBookingPage() {
-  redirect(process.env.GOOGLE_BOOKING_URL || FALLBACK_BOOKING_URL);
+  redirect(process.env.CAL_BOOKING_URL || FALLBACK_BOOKING_URL);
 }
